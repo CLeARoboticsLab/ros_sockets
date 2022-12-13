48e60143c34a3166b5c69d29a05272ef339c8dc5
jake-levy <jake.j.levy@gmail.com>
2022-12-13 13:42:32 -0800

    pub to /<tracker_name>/pose topic; fix indentation

--- src/gazebo_tracker.cpp
+++ src/gazebo_tracker.cpp
@@ -1,71 +1,102 @@
 #include <ros/ros.h>
 #include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseStamped.h"
 #include <gazebo_msgs/ModelStates.h>
 #include <string>
 
-class Tracker {
+// Tracks the pose of a model in Gazebo, <model_name> and publishes it to
+// /<tracker_name>/pose.
+class Tracker 
+{
+  public:
 
-    public:
+    Tracker()
+    {
+      model_idx_ = -1;
+      ready_to_pub_ = false;
 
-        Tracker() {
-            model_idx_ = -1;
-            ready_to_pub_ = false;
+      nh_.getParam("/gazebo_tracker/tracker_name", tracker_name_);
+      nh_.getParam("/gazebo_tracker/model_name", model_name_);
+      double update_frequency;
+      nh_.getParam("/gazebo_tracker/update_frequency", update_frequency);
+      
+      std::string pub_topic = "/" + tracker_name_ + "/pose";
+      pub_ = nh_.advertise<geometry_msgs::PoseStamped>(
+          pub_topic, 100);
+      sub_ = nh_.subscribe(
+          "/gazebo/model_states", 100,
+          &Tracker::model_states_callback, this);
+      timer_ = nh_.createTimer(
+          ros::Duration(1.0/update_frequency),
+          &Tracker::timerCallback, this);
+    }
 
-            double update_frequency;
-            nh_.getParam("/gazebo_tracker/update_frequency", update_frequency);
-            nh_.getParam("/gazebo_tracker/model_name", model_name_);
+  private:
 
-            timer_ = nh_.createTimer(
-                ros::Duration(1.0/update_frequency),
-                &Tracker::timerCallback, this);
-            sub_ = nh_.subscribe(
-                "/gazebo/model_states", 100,
-                &Tracker::model_states_callback, this);
-            // TODO: publisher
-        }
+    // General information
+    std::string tracker_name_;
+    std::string model_name_;
+    int model_idx_;
+    
+    // Pose data
+    geometry_msgs::Pose model_pose_;
+    geometry_msgs::PoseStamped model_pose_stamped_;
 
-    private:
+    // Used to control the rate that pose data is published
+    bool ready_to_pub_;
 
-        std::string model_name_;
-        int model_idx_;
-        bool ready_to_pub_;
-        geometry_msgs::Pose model_pose_;
+    // ROS objects
+    ros::NodeHandle nh_;
+    ros::Publisher pub_;
+    ros::Subscriber sub_;
+    ros::Timer timer_;
 
-        ros::NodeHandle nh_;
-        ros::Timer timer_;
-        ros::Subscriber sub_;
+    // Returns the index of string s in vector v
+    int getModelIndex(std::vector<std::string> v, std::string s)
+    {
+      for(int i = 0; i < v.size(); i++)
+      {
+        if(v[i].compare(s) == 0)
+          return i;
+      }
+      return -1;
+    }
 
-        int getModelIndex(std::vector<std::string> v, std::string value) {
-            for(int i = 0; i < v.size(); i++) {
-                if(v[i].compare(value) == 0)
-                    return i;
-            }
-            return -1;
-        }
+    // Updates the model index and publishes pose data if ready to publish.
+    void model_states_callback(gazebo_msgs::ModelStates model_states)
+    {
+      model_idx_ = getModelIndex(model_states.name, model_name_);
+      if (ready_to_pub_ && model_idx_ != -1)
+      {
+        model_pose_ = model_states.pose[model_idx_];
+        publish_pose_stamped();
+      }
+    }
+    
+    // When the timer triggers, set the object ready to publish. 
+    void timerCallback(const ros::TimerEvent&)
+    {
+      ready_to_pub_ = true;
+    }
 
-        void model_states_callback(gazebo_msgs::ModelStates model_states) {
-            model_idx_ = getModelIndex(model_states.name, model_name_);
-            if (!ready_to_pub_ || model_idx_ == -1) {
-                return;
-            }
-            ready_to_pub_ = false;
-            model_pose_ = model_states.pose[model_idx_];
-            ROS_INFO_STREAM(model_pose_); //TODO: debug stream
-        }
-
-        void timerCallback(const ros::TimerEvent&) {
-            ready_to_pub_ = true;
-        } 
+    // Publishes the pose data to the topic.
+    void publish_pose_stamped()
+    {
+      ready_to_pub_ = false;
+      model_pose_stamped_.pose = model_pose_;
+      pub_.publish(model_pose_stamped_);
+    } 
 
 };
 
-int main(int argc, char** argv) {
-    
-    ros::init(argc, argv, "gazebo_tracker");
+int main(int argc, char** argv)
+{
+  
+  ros::init(argc, argv, "gazebo_tracker");
 
-    Tracker tracker;
+  Tracker tracker;
 
-    ros::spin();
-    
-    return 0;
+  ros::spin();
+  
+  return 0;
 }
